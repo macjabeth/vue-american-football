ab99dbdd656003fd5d1d7e51ab1fc41efae253f4
macjabeth <macbethjonathan@gmail.com>
2020-05-12 22:54:42 -0400

    set up methods and interval timer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Scoreboard :score="score" :time="time" />
+    <Controls :incrementScore="incrementScore" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Scoreboard from '@/components/Scoreboard';
+  import Controls from '@/components/Controls';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: { Controls, Scoreboard},
+    mounted() {
+      this.timer = this.$nextTick(() => {
+        setInterval(this.incrementTime, 1000);
+      });
+    },
+    destroyed() {
+      clearInterval(this.timer);
+    },
+    data() {
+      return {
+        score: { home: 32, guest: 3 },
+        time: { minute: 0, second: 3 }
+      }
+    },
+    methods: {
+      incrementScore(team, amount) {
+        this.score[team] += amount;
+      },
+      incrementTime() {
+        if (++this.time.second === 60) {
+          this.time.second = 0;
+          this.time.minute++;
+        }
+      }
+    }
+  };
 </script>
 
 <style>
